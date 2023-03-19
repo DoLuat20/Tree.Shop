65912978df1c0ec264527edaf5f44a6d73e62a34
Đỗ Luật <doluats20@gmail.com>
2023-03-20 00:27:50 +0700

    upload tree shop

--- js/connectFirebase.js
+++ js/connectFirebase.js
@@ -1,114 +1,116 @@
 var app = angular.module("app", ["firebase"]);
-app.config(function() {
+app.config(function () {
     var config = {
         apiKey: "AIzaSyDDx-jBYOpSVMxIu2UfbV3yqD0XyikZsvw",
         authDomain: "workspace-b1838.firebaseapp.com",
         databaseURL: "https://workspace-b1838-default-rtdb.firebaseio.com",
-        storageBucket: "workspace-b1838.appspot.com"
+        storageBucket: "workspace-b1838.appspot.com",
     };
     // JavaScript
 
     firebase.initializeApp(config);
 });
-app.controller("MyCtrl", ["$scope", "$firebaseArray",
-    function($scope, $firebaseArray) {
-        var ref = firebase.database().ref('/product');
+app.controller("MyCtrl", [
+    "$scope",
+    "$firebaseArray",
+    function ($scope, $firebaseArray) {
+        var ref = firebase.database().ref("/product");
         var list = $firebaseArray(ref);
-        var ref2 = firebase.database().ref('/account')
+        var ref2 = firebase.database().ref("/account");
         var list2 = $firebaseArray(ref2);
-        var id = new URL(document.URL).searchParams.get('id');
-        $scope.items = []
+        var id = new URL(document.URL).searchParams.get("id");
+        $scope.items = [];
         list.$loaded()
-            .then(function(x) {
-                x.forEach((v) => {
+            .then(function (x) {
+                x.forEach(v => {
                     if (id) {
-                        $scope.item = x.filter((x) => x.$id === id)
+                        $scope.item = x.filter(x => x.$id === id);
                     }
-                    $scope.items = x
+                    $scope.items = x;
                     $scope.begin = 0;
                     $scope.pageCount = Math.ceil($scope.items.length / 6);
                     $scope.first = () => {
                         $scope.begin = 0;
-                    }
+                    };
                     $scope.prev = () => {
                         if ($scope.begin > 0) {
                             $scope.begin -= 6;
                         }
-                    }
+                    };
                     $scope.next = () => {
                         if ($scope.begin < ($scope.pageCount - 1) * 6) {
                             $scope.begin += 6;
                         }
-                    }
+                    };
                     $scope.last = () => {
                         $scope.begin = ($scope.pageCount - 1) * 6;
-                    }
-                })
+                    };
+                });
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log("Error:", error);
             });
-        list2.$loaded()
-            .then(function(x) {
+        list2
+            .$loaded()
+            .then(function (x) {
                 $scope.accountList = x;
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log("Error:", error);
             });
-        $scope.cart = JSON.parse(localStorage.getItem("Cart") || '[]');
+        $scope.cart = JSON.parse(localStorage.getItem("Cart") || "[]");
         $scope.total = localStorage.getItem("total");
 
-
-        $scope.addCart = (sp) => {
-            var index = $scope.cart.findIndex((item) => {
-                return (item.sanpham.$id == sp.$id);
+        $scope.addCart = sp => {
+            var index = $scope.cart.findIndex(item => {
+                return item.sanpham.$id == sp.$id;
             });
             if (index < 0) {
                 var newSP = {
                     sanpham: sp,
-                    slBan: 1
+                    slBan: 1,
                 };
                 $scope.cart.push(newSP);
             } else {
-                $scope.cart[index].slBan++
+                $scope.cart[index].slBan++;
             }
             $scope.total++;
             localStorage.setItem("total", JSON.stringify($scope.total));
             $scope.total = localStorage.getItem("total");
             localStorage.setItem("Cart", JSON.stringify($scope.cart));
-            $scope.cart = JSON.parse(localStorage.getItem("Cart") || '[]');
-        }
+            $scope.cart = JSON.parse(localStorage.getItem("Cart") || "[]");
+        };
 
-        $scope.increase = (sp) => {
+        $scope.increase = sp => {
             $scope.cart.forEach(x => {
                 if (x.sanpham.$id === sp.sanpham.$id) {
                     x.slBan++;
                     $scope.total++;
                 }
-            })
+            });
             localStorage.setItem("total", JSON.stringify($scope.total));
             localStorage.setItem("Cart", JSON.stringify($scope.cart));
-        }
+        };
 
-        $scope.decrease = (sp) => {
+        $scope.decrease = sp => {
             if (sp.slBan > 1) {
                 $scope.cart.forEach(x => {
                     if (x.sanpham.$id === sp.sanpham.$id) {
                         x.slBan--;
                         $scope.total--;
                     }
-                })
+                });
                 localStorage.setItem("total", JSON.stringify($scope.total));
                 localStorage.setItem("Cart", JSON.stringify($scope.cart));
             }
-        }
+        };
 
-        $scope.removeCart = (sp) => {
-            $scope.cart = $scope.cart.filter(x => x.sanpham.$id !== sp.sanpham.$id)
-            $scope.total -= sp.slBan
+        $scope.removeCart = sp => {
+            $scope.cart = $scope.cart.filter(x => x.sanpham.$id !== sp.sanpham.$id);
+            $scope.total -= sp.slBan;
             localStorage.setItem("total", JSON.stringify($scope.total));
             localStorage.setItem("Cart", JSON.stringify($scope.cart));
-        }
+        };
 
         $scope.subtotal = () => {
             var subtotal = 0;
@@ -118,7 +120,7 @@ app.controller("MyCtrl", ["$scope", "$firebaseArray",
                 }
             }
             return subtotal;
-        }
+        };
 
         $scope.addAccount = () => {
             var account = {};
@@ -128,8 +130,8 @@ app.controller("MyCtrl", ["$scope", "$firebaseArray",
             account.password = $scope.password;
             list2.$add(account);
             alert("Ban da dang ki tai khoan thanh cong!!!");
-            location.replace('/Bootstrap/Asm/sign-in.htmll');
-        }
+            location.replace("/Bootstrap/Asm/sign-in.htmll");
+        };
 
         $scope.users = sessionStorage.getItem("user");
 
@@ -138,22 +140,22 @@ app.controller("MyCtrl", ["$scope", "$firebaseArray",
                 if ($scope.phone == $scope.accountList[i].sdt) {
                     if ($scope.password == $scope.accountList[i].password) {
                         $scope.isLogin = true;
-                        $scope.user = $scope.accountList[i]
+                        $scope.user = $scope.accountList[i];
                         if ($scope.RememberMe) {
-                            sessionStorage.setItem("user", JSON.stringify($scope.user))
+                            sessionStorage.setItem("user", JSON.stringify($scope.user));
                         }
-                        location.replace('/');
-                        alert('đăng nhập thành công');
+                        location.replace("/Tree.Shop");
+                        alert("đăng nhập thành công");
                         break;
                     } else {
-                        alert('Sai mật khẩu')
+                        alert("Sai mật khẩu");
                         break;
                     }
                 } else {
-                    alert('Sai tên đăng nhập')
+                    alert("Sai tên đăng nhập");
                     break;
                 }
             }
-        }
-    }
+        };
+    },
 ]);
